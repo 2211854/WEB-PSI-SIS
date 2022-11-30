f18300b2df485aa0d374f3e8050919e8c77e748f
2211853 <2211853@my.ipleiria.pt>
2022-11-30 22:15:35 +0000

    implementação do frontend

--- mjram/frontend/assets/AppAsset.php
+++ mjram/frontend/assets/AppAsset.php
@@ -13,8 +13,36 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        'css/linearicons.css',
+        'css/font-awesome.min.css',
+        'css/themify-icons.css',
+        'css/bootstrap.css',
+        'css/owl.carousel.css',
+        'css/nice-select.css',
+        'css/nouislider.min.css',
+        'css/ion.rangeSlider.css',
+        'css/ion.rangeSlider.skinFlat.css',
+        'css/magnific-popup.css',
+        'css/main.css',
+        'css/bootstrap.min.css',
+        'css/bootstrap-datepicker.min.css',
+
     ];
     public $js = [
+        'js/vendor/jquery-2.2.4.min.js',
+        'js/vendor/popper.min.js',
+        'js/vendor/bootstrap.min.js',
+        'js/jquery.ajaxchimp.min.js',
+        'js/jquery.nice-select.min.js',
+        'js/jquery.sticky.js',
+        'js/nouislider.min.js',
+        'js/jquery.magnific-popup.min.js',
+        'js/bootstrap.bundle.min.js',
+        'js/bootstrap-datepicker.min.js',
+        'js/bootstrap-datepicker.pt.js',
+        'js/owl.carousel.min.js',
+        'js/gmaps.min.js',
+        'js/main.js'
     ];
     public $depends = [
         'yii\web\YiiAsset',
