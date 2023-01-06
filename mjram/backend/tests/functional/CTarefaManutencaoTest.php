8023e635d4cb3c07d563a86e8d9eb25d434f4c41
Ricardo santos <2211925@my.ipleiria.pt>
2023-01-06 12:45:05 +0000

    Criacao de todos os tests
    Criacao de todos os tests sem ser o de acceptance.
    codigo logincest backend feito

---
+++ mjram/backend/tests/functional/CTarefaManutencaoTest.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace backend\tests\Functional;
+
+use backend\tests\FunctionalTester;
+
+class CTarefaManutencaoTest extends \Codeception\Test\Unit
+{
+
+    protected FunctionalTester $tester;
+
+    protected function _before()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+
+    }
+}
