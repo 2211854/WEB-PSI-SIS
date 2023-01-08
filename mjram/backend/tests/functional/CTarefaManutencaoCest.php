cf32dd46275c6b9adf063732f6417e3d367919ba
ricardosantos2003 <2211925@my.ipleiria.pt>
2023-01-08 02:56:18 +0000

    functional testes
    functional testes todos feitos (falta so ligeiramente o comprar ticket)

--- mjram/backend/tests/functional/CTarefaManutencaoCest.php
+++ mjram/backend/tests/functional/CTarefaManutencaoCest.php
@@ -9,10 +9,19 @@ class CTarefaManutencaoCest
 {
     public function _before(FunctionalTester $I)
     {
+        $admin = \common\models\User::findByUsername('administrador');
+        $I->amLoggedInAs($admin);
     }
 
     // tests
-    public function tryToTest(FunctionalTester $I)
+    public function CreateTarefa(FunctionalTester $I)
     {
+        $I->amOnRoute('/tarefa/create?vooid=1');
+        $I->amGoingTo('Tentar colocar tudo');
+        $I->fillField('Tarefa[designacao]', 'Limpeza aviao');
+        $I->selectOption('Tarefa[estado]', 'Planeada');
+        $I->fillField('Tarefa[id_hangar]', 'Norte 1');
+        $I->fillField('Tarefa[id_recurso]', 'Vasssoura');
+        $I->click('Save');
     }
 }
